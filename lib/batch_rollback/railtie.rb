6f2e25f9860c2d0d7db64275f624b508684e5bf1
Naoto Kaneko <naoty.k@gmail.com>
2018-11-11 10:43:36 +0900

    Hook around db:migrate and db:rollback

--- lib/batch_rollback/railtie.rb
+++ lib/batch_rollback/railtie.rb
@@ -1,13 +1,33 @@
 module BatchRollback
   class Railtie < Rails::Railtie
     rake_tasks do
-      namespace :db do
-        task :migrate do
-          MigrationStep.create_table
+      namespace :batch_rollback do
+        task :pre_migrate do
+          puts "TODO: pre_migrate"
         end
 
-        task :rollback do
-          puts "TODO: hook db:rollback"
+        task :post_migrate do
+          puts "TODO: post_migrate"
+        end
+
+        task :pre_rollback do
+          puts "TODO: pre_rollback"
+        end
+
+        task :post_rollback do
+          puts "TODO: post_rollback"
+        end
+      end
+
+      if Rake::Task.task_defined?("db:migrate")
+        Rake::Task["db:migrate"].enhance(["batch_rollback:pre_migrate"]) do
+          Rake::Task["batch_rollback:post_migrate"].invoke
+        end
+      end
+
+      if Rake::Task.task_defined?("db:rollback")
+        Rake::Task["db:rollback"].enhance(["batch_rollback:pre_rollback"]) do
+          Rake::Task["batch_rollback:post_rollback"].invoke
         end
       end
     end
